7c93f38f673bed1566a5dffa25da126892bac149
LeChuck42 <github@mseidel.net>
2013-09-12 19:07:53 +0200

    accedently committed wrong file... :) this one should do!

--- 7seg.c
+++ 7seg.c
@@ -1,18 +1,29 @@
-#include <avr/io.h>
-#include <util/delay.h>
-#include <stdlib.h>
-#include "7seg.h"
+/*
+ * 7seg.c
+ *
+ *  Created on: 31.10.2012
+ *      Author: PC 8
+ */
 
-//static const number[10] = {};
+#include "7seg.h"
+#include <avr/io.h>
+#include <stdlib.h>
+#include <util/delay.h>
 
 static void shift_bit(unsigned char d);
-static void write_digit(unsigned char seg, unsigned char data, unsigned char erase);
+static void write_digit(unsigned char seg, unsigned char data, unsigned char erase, unsigned char skip);
+static void shift_mech();
+static void skip_bit();
+static void clear_digit(unsigned char digit, unsigned char skip);
+static void set_digit(unsigned char digit, unsigned char c, unsigned char skip);
+
+
+static unsigned char content[14];
 
 static void shift_bit(unsigned char d)
 {
 	CLR_CLK; // CLK down
 
-	WAIT_FULL(DATA_DELAY);
 	if (d)
 	{
 		SET_DATA;
@@ -21,69 +32,113 @@ static void shift_bit(unsigned char d)
 	{
 		CLR_DATA;
 	}
-	WAIT_FULL(DATA_DELAY);
-	SET_CLK; // CLK up - shift bit
 
-	WAIT_FULL(DATA_DELAY);
+	_delay_us(DATA_DELAY);
+	SET_CLK; // CLK up - shift bit
+	_delay_us(DATA_DELAY);
 }
 
-static void shift_mech(unsigned char d)
+static void skip_bit()
 {
-	CLR_CLK; // CLK down
+	SET_CLK; // CLK down
+	_delay_us(1);
+	CLR_CLK; // CLK up - shift bit
+	_delay_us(1);
+}
 
-	WAIT_FULL(MECH_DELAY);
-	if (d)
-	{
-		SET_DATA;
-	}
-	else
-	{
-		CLR_DATA;
-	}
-	WAIT_FULL(MECH_DELAY);
-	SET_CLK; // CLK up - shift bit
-
-	WAIT_FULL(MECH_DELAY);
+static void shift_mech()
+{
+	SET_CLK; // CLK down
+	WAIT_HALF(MECH_DELAY);
+	CLR_CLK; // CLK up - shift bit
+	WAIT_HALF(MECH_DELAY);
 }
 
 
 
-static void write_digit(unsigned char seg, unsigned char data, unsigned char erase)
+static void write_digit(unsigned char seg, unsigned char data, unsigned char erase, unsigned char skip)
 {
+	static const unsigned char delete_order[7]={SEG_U,SEG_UR,SEG_UL,SEG_M,SEG_OR,SEG_OL,SEG_O};
+	static const unsigned char set_order[7]={SEG_O,SEG_OR,SEG_UR,SEG_U,SEG_UL,SEG_OL,SEG_M};
+
 	unsigned char i;
 
-	SET_STROBE;
-
-	WAIT_FULL(DATA_DELAY);
-	// pump data
-
-	for (i=0x80; i; i>>=1)
+	if ((skip & SEG_ALL) != SEG_ALL)
 	{
-		shift_bit(data&i);
+		SET_STROBE;
+
+		WAIT_FULL(DATA_DELAY);
+		// pump data
+
+		for (i=0x80; i; i>>=1)
+		{
+			shift_bit(data&i);
+		}
+
+		// pump address
+
+
+		for (i=0x80; i; i>>=1)
+		{
+			shift_bit(seg&i);
+		}
+
+		CLR_STROBE; // falling strobe, latch data
+
+		WAIT_HALF(DATA_DELAY);
+
+		CLR_CLK;
+
+		WAIT_HALF(DATA_DELAY);
+
+		// enable segments??
+		if (erase)
+		{
+			CLR_DATA;
+			for (i=0; i<7; i++)
+			{
+				if (skip&delete_order[i])
+				{
+					skip_bit();
+				}
+				else
+				{
+					shift_mech();
+				}
+			}
+		}
+		else
+		{
+			SET_DATA;
+			for (i=0; i<7; i++)
+			{
+				if (skip&set_order[i])
+				{
+					skip_bit();
+				}
+				else
+				{
+					shift_mech();
+				}
+			}
+		}
+		SET_CLK;
 	}
+}
 
-	// pump address
-
-
-	for (i=0x80; i; i>>=1)
-	{
-		shift_bit(seg&i);
-	}
-
-	CLR_STROBE; // falling strobe, latch data
-
-	WAIT_FULL(DATA_DELAY);
-
-
-	// enable segments??
-	for (i=0x80; i; i>>=1)
-	{
-		shift_mech(erase&i);
-	}
+void Init_7seg()
+{
+	unsigned char i;
+	DDRD |= 0x07;
+	PORTA = 0;
+	for (i=0; i<14; i++)
+		content[i] = SEG_ALL;
+	for (i=1; i<=14; i++)
+		clear_digit(i, 0);
 
 }
 
-static const unsigned char char_to_seg(unsigned char c)
+const unsigned char char_to_seg(unsigned char c)
 {
 	switch (c)
 	{
@@ -205,48 +260,149 @@ static const unsigned char char_to_seg(unsigned char c)
 	}
 }
 
-static void clear_seg(unsigned char seg)
+static void clear_digit(unsigned char digit, unsigned char skip)
 {
-	if (seg >= 1 && seg <= 14 )
+	if (digit >= 1 && digit <= 14 )
 	{
-		seg <<= 4;
-		write_digit(seg,0x00,0x00);
+		skip |= ~content[digit-1];
+		write_digit(digit<<4, 0x00, 1, skip);
+		content[digit-1] &= skip;
 	}
 }
 
-static void set_seg(unsigned char seg, unsigned char c)
+static void set_digit(unsigned char digit, unsigned char c, unsigned char skip)
 {
-	if (seg >= 1 && seg <= 14 )
+	if (digit >= 1 && digit <= 14 )
 	{
-		write_digit(seg,c,0xFF);
+		skip |= content[digit-1];
+		write_digit(digit, c, 0, skip);
+		content[digit-1] |= c & ~skip;
 	}
 }
 
-static unsigned char write_str( char* str, unsigned char start)
+
+void write_seg( unsigned char digit, unsigned char seg)
 {
+	seg &= SEG_ALL;
+	clear_digit(digit, seg);
+	set_digit(digit, seg, ~seg);
+}
+
+void write_char( unsigned char digit, unsigned char c)
+{
+	c=char_to_seg(c);
+	clear_digit(digit, c);
+	set_digit(digit, c, ~c);
+}
+
+
+unsigned char write_str( char* str, unsigned char start)
+{
+	unsigned char c;
 	while (*str)
 	{
-		clear_seg(start);
-		set_seg(start++,char_to_seg(*str++));
+		c=char_to_seg(*str++);
+		clear_digit(start, c);
+		set_digit(start++, c, ~c);
 		if (start > 14)
 			start = 1;
 	}
 	return start;
 }
 
-static unsigned char write_str_roll( char* str, unsigned char start, unsigned char end, unsigned char pos)
+unsigned char write_str_roll( char* str, unsigned char start, unsigned char end, unsigned char pos)
 {
 	unsigned char local_pos=pos++;
+	unsigned char c;
 	for (;start <= end; start++, local_pos++)
 	{
 		if (*(str+local_pos) == 0)
 		{
 			local_pos=0;
 		}
-		clear_seg(start);
-		set_seg(start,char_to_seg(*(str+local_pos)));
+		c = char_to_seg(*(str+local_pos));
+		clear_digit(start, c);
+		set_digit(start,c,~c);
 	}
 	if (*(str+pos) == 0)
 		pos = 0;
 	return pos;
+}
+
+unsigned char write_seg_roll( char* str, unsigned char start, unsigned char end, unsigned char pos)
+{
+	unsigned char local_pos=pos++;
+	unsigned char c;
+	for (;start <= end; start++, local_pos++)
+	{
+		if (*(str+local_pos) == 0)
+		{
+			local_pos=0;
+		}
+		c = *(str+local_pos);
+		clear_digit(start, c);
+		set_digit(start,c,~c);
+	}
+	if (*(str+pos) == 0)
+		pos = 0;
+	return pos;
+}
+
+unsigned char write_str_blend(char* str, unsigned char start)
+{
+	static const unsigned char bit_count[16] = {0,1,1,2,1,2,2,3,1,2,2,3,2,3,3,4};
+	unsigned char diff[14];
+
+	unsigned char seg_num;
+	unsigned char seg,digit;
+	unsigned char str_len, seg_count;
+
+	/* empty str */
+	if (*str == 0)
+		return 0;
+
+	seg_count = 0;
+	str_len = 0;
+
+	for (str_len=0;*str;str++)
+	{
+		diff[str_len] = char_to_seg(*str) ^ content[start+str_len-1];
+		seg_count += bit_count[diff[str_len]&0x0f];
+		seg_count += bit_count[diff[str_len]>>4];
+		str_len++;
+	}
+
+	if (seg_count == 0)
+		return 0;
+
+	seg_num = rand();
+
+	if (seg_count > 1)
+		seg_num %= seg_count;
+	else
+		seg_num = 0;
+
+	for (digit=0;digit<str_len;digit++)
+	{
+		if (diff[digit] != 0)
+		{
+			for (seg=0x01; seg; seg<<=1)
+			{
+				if (diff[digit] & seg)
+				{
+					if (seg_num == 0)
+					{
+						/* update this segment */
+						write_seg(digit+start,content[digit+start-1]^seg);
+						return 1;
+					}
+					else
+					{
+						seg_num--;
+					}
+				}
+			}
+		}
+	}
+	return 0;
 }
